2c4d32ec09cd6411a4951d4abba3f4327f6df923
Ian Pedro <ianpedro7@gmail.com>
2023-06-13 01:33:47 -0300

    UP-google-auth-firebase

--- upskills/src/actions/index.js
+++ upskills/src/actions/index.js
@@ -1,11 +1,13 @@
 import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
+
 
 const signInAPI = () => {
     return (dispatch) => {
-        auth.signInWithPopup(provider).then((payload) => {
-            console.log(payload)
-        })
-        .catch((error) => alert(error.message));
+        signInWithPopup(auth, provider)
+        .then((payload) => {
+          console.log(payload)
+        }).catch((error) => alert(error.message));
     }
 }
 
